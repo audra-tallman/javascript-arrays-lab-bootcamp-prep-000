4af647c8e890d4c42dde36f84b8c961764c41dec
Audra Tallman <audra.tallman@hotmail.com>
2019-02-11 20:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function destructivelyAppendKitten(name) {
   kittens.push("Ralph")
     return kittens
 }
-
 function destructivelyPrependKitten(name) {
   kittens.unshift("Bob")
     return kittens
@@ -14,18 +13,15 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop()
     return kittens
 }
-
 function destructivelyRemoveFirstKitten(name) {
   kittens.shift()
     return kittens
 }
-
 function  appendKitten(name) {
   var kittens = ["Milo", "Otis", "Garfield"]
   var kitten2 = ["Broom"]
     return kittens.concat(kitten2)
 }
-
 function  prependKitten(name) {
   var kittens = ["Milo", "Otis", "Garfield"]
   var kitten3 = ["Arnold"]
